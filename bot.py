2b3aaec97c24b43c03b69855517bcc72f91c02ca
vladodio <47479407+vladodio@users.noreply.github.com>
2020-10-15 17:22:03 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+
 # loads the token from the .env file
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -15,14 +16,76 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=discord.Game("Studying..."))
     print(f'{bot.user} has connected to Discord!')
 
+# unfinished function to list all available gears 
+'''
+@bot.command()
+async def listCogs(ctx, extension):
+    outStrActive = [""]
+    outStrDisabled = [""]
+    for filename in os.listdir('./cogs'):
+        if(filename.endswith('.py')):
+            outStrActive[]
+        elif(filename.endswith('.py.disabled')):
+'''
+# commands for loading and unloading cogs once the bot is running
 @bot.command()
 async def unload(ctx, extension):
-    bot.unload_extension(f'cogs.{extension}')
+    extension = extension.lower()
+    if(not (extension == "all")):
+        try:
+            bot.unload_extension(f'cogs.{extension}')
+            await ctx.send(f'Cog {extension} successfully unloaded')
+        except commands.errors.ExtensionNotLoaded:
+            await ctx.send(f'Cog {extension} was not initally loaded')
+    else:
+        for filename in os.listdir('./cogs'):
+            if( filename.endswith('.py')):
+                try:
+                    bot.unload_extension(f'cogs.{filename[:-3]}')
+                except commands.errors.ExtensionNotLoaded:
+                    pass
+        await ctx.send('All cogs successfully unloaded')
+
 
 @bot.command()
 async def load(ctx, extension):
-    bot.load_extension(f'cogs.{extension}')
+    extension = extension.lower()
+    if(not (extension == "all")):
+        try:
+            bot.load_extension(f'cogs.{extension}')
+            await ctx.send(f'Cog {extension} successfully loaded')
+        except discord.ext.commands.errors.ExtensionNotFound:
+            await ctx.send(f'Cog {extension} not found')
+    else:
+        for filename in os.listdir('./cogs'):
+            if( filename.endswith('.py')):
+                bot.load_extension(f'cogs.{filename[:-3]}')
+        await ctx.send('All cogs successfully loaded')
 
+@bot.command()
+async def reload(ctx, extension):
+    await unload(ctx, extension)
+    await load(ctx, extension)
+
+# commands for disabling and enabling cogs
+@bot.command()
+async def disableCog(ctx, extension):
+    extension = extension.lower()
+    await unload(ctx, extension)
+    if(os.path.isfile(f'./cogs/{extension}.py')):
+        os.rename(f'./cogs/{extension}.py',f'./cogs/{extension}.py.disabled')
+        await ctx.send(f'Cog {extension} successfully disabled')
+    else:
+        pass
+
+@bot.command()
+async def enableCog(ctx, extension):
+    extension = extension.lower()
+    if(os.path.isfile(f'./cogs/{extension}.py.disabled')):
+        os.rename(f'./cogs/{extension}.py.disabled',f'./cogs/{extension}.py')
+        await ctx.send(f'Cog {extension} successfully enabled')
+    else:
+        pass
 
 # simple ping command
 @bot.command()
@@ -34,5 +97,6 @@ async def ping(ctx):
 for filename in os.listdir('./cogs'):
     if( filename.endswith('.py')):
         bot.load_extension(f'cogs.{filename[:-3]}')
+
 # runs the bot
 bot.run(TOKEN)
