aa5980c75e8c9261575e6f39e6ab882704ca81ce
vladodio <vladodio@yahoo.com>
2021-01-04 01:12:47 -0500

    Improper indentation was used in file.

--- cogs/clear.py
+++ cogs/clear.py
@@ -3,18 +3,17 @@ from discord.ext import commands
 
 class Clear(commands.Cog):
 
-        def __init__(self, bot):
-                self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-        @commands.Cog.listener()
-        async def on_ready(self):
-                pass
+    @commands.Cog.listener()
+    async def on_ready(self):
+        pass
 
-        @commands.command()
-        async def clear(self, ctx, amount=5):
-                await ctx.channel.purge(limit=amount)
+    @commands.command()
+    async def clear(self, ctx, amount=5):
+        await ctx.channel.purge(limit=amount)
 
 
 def setup(bot):
-        bot.add_cog(Clear(bot))
-
+    bot.add_cog(Clear(bot))
