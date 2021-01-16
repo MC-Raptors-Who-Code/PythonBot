a6085305b4cc3d8430aee08f994b601d54bac9d6
vladodio <vladodio@yahoo.com>
2021-01-16 13:41:33 -0500

    Updating to be in line with linter.

--- cogs/clear.py
+++ cogs/clear.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 
 class Clear(commands.Cog):
 
+
     def __init__(self, bot):
         self.bot = bot
 
@@ -13,7 +14,6 @@ class Clear(commands.Cog):
     @commands.command()
     async def clear(self, ctx, amount=5):
         await ctx.channel.purge(limit=amount)
-
 
 def setup(bot):
     bot.add_cog(Clear(bot))
